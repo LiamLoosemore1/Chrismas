d347acc8bdcaeffb129e6116d1df3fb93e198128
liam <474967@chrome.escg.ac.uk>
2019-12-19 14:04:06 +0000

    Added particles & collecatbles & scoring system.

---
+++ chrismas-project/Assets/Scripts/Collectables/Collecatbles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collecatbles : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        ScoringSystem.score += 1;
+
+        Destroy(gameObject);
+
+    }
+
+}
