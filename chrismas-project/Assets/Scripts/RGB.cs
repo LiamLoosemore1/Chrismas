decd2c9fdb885226556d3c7ace6c78ffaf18ff66
Liam loosemore <474967@chrome.escg.ac.uk>
2019-12-19 14:57:29 +0000

    Final game

---
+++ chrismas-project/Assets/Scripts/RGB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RGB : MonoBehaviour
+{
+    const float speed = 0.5f;
+    float t = 0;
+    int i = 0;
+    Color[] colors = {
+        Color.white,
+        Color.red,
+        Color.green,
+        Color.cyan
+    };
+
+    void Update()
+    {
+        t += Time.deltaTime * speed;
+        gameObject.GetComponent<Light>().color = Color.Lerp(colors[i], colors[i + 1], t);
+
+        if (t > 1)
+        {
+            t = 0;
+            i++;
+            if (i >= colors.Length - 1) i = 0;
+        }
+    }
+}
